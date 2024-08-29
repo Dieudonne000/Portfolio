2c66407aefb7d5d12a2095f8ea9872eedd024ddf
MUNEZA Jean Dieudonne <munezadieudonne2021@gmail.com>
2024-08-29 15:21:52 +0200

    About section

--- app/src/components/about.jsx
+++ app/src/components/about.jsx
@@ -1,9 +1,11 @@
 import './nav.css'
+import me from '/public/Moi.jpg'
 
 function About() {
     return (
         <div className='about'>
-        <h1 className='Ac'>About me</h1>
+            <h1 className='Ac'>About me</h1>
+            <img src={me} alt="my pic" className='pic' />
         </div>
     )
 }
--- app/src/components/hero.jsx
+++ app/src/components/hero.jsx
@@ -2,16 +2,20 @@
 import './nav.css'
 import arrows from '/public/Arrows.svg'
 import arc from '/public/arc.svg'
+import tri from '/public/triangle.svg'
+import crs from '/public/cross.svg'
 
 function Hero() {
     return (
         <div className='Hero'>
             <img src={arrows} alt="background-shape" className='arrows' />
+            <img src={crs} alt="background-shape" className='crs' />
             <h1>
             Hi.I'm Dieudonne.<br></br>
             A Web developer 
             </h1>
             <img src={arc} alt="background-shape" className='arc' />
+            <img src={tri} alt="background-shape" className='tri' />
             <p>Hi I'm Passionate about creating outstanding webapps</p>
         </div>
     )
